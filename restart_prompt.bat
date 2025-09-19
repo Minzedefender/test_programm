60b075d54131a99c2a401c4cf931e4fa5962891c
Minzedefender <djminze@bk.ru>
2025-09-19 12:01:51 +0300

    Add restart prompt batch script

---
+++ restart_prompt.bat
@@ -0,0 +1,18 @@
+﻿@echo off
+chcp 65001 >nul
+setlocal
+
+choice /c YN /m "Перезапустить компьютер сейчас?"
+if errorlevel 2 goto no
+if errorlevel 1 goto yes
+
+:yes
+echo Перезапуск...
+shutdown /r /t 0
+goto end
+
+:no
+echo Перезапуск отменен.
+
+:end
+endlocal
